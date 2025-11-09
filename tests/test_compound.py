10c6b47df8b93af48234a4a4810eabac85787316
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2025-11-09 12:49:44 +0000

    Fix test names for updated matcher names

--- tests/test_compound.py
+++ tests/test_compound.py
@@ -4,51 +4,51 @@ from joythief.compound import AllOf, AnyOf
 from joythief.strings import JsonString, StringMatching
 
 
-def test_all_false_if_none_match():
+def test_allof_false_if_none_match():
     assert "foo" != AllOf(StringMatching(r"^{}$"), JsonString())
 
 
-def test_all_false_if_any_do_not_match():
+def test_allof_false_if_any_do_not_match():
     assert "foo" != AllOf(StringMatching(r"fo+"), JsonString())
 
 
-def test_all_true_if_all_match():
+def test_allof_true_if_all_match():
     assert "{}" == AllOf(StringMatching(r"^{}$"), JsonString())
 
 
-def test_all_repr_shows_matches():
+def test_allof_repr_shows_matches():
     matcher = AllOf(JsonString(), StringMatching(r"fo+"))
     assert "foo" != matcher
     assert repr(matcher) == "AllOf(JsonString(), 'foo')"
 
 
-def test_all_requires_some_matchers():
+def test_allof_requires_some_matchers():
     with pytest.raises(TypeError):
         _ = AllOf()
 
 
-def test_all_warns_on_single_matcher():
+def test_allof_warns_on_single_matcher():
     with pytest.warns(UserWarning):
         _ = AllOf(JsonString())
 
 
-def test_any_false_if_none_match():
+def test_anyof_false_if_none_match():
     assert "foo" != AnyOf(StringMatching(r"^{}$"), JsonString())
 
 
-def test_any_true_if_any_match():
+def test_anyof_true_if_any_match():
     assert "foo" == AnyOf(StringMatching(r"fo+"), JsonString())
 
 
-def test_any_true_if_all_match():
+def test_anyof_true_if_all_match():
     assert "{}" == AnyOf(StringMatching(r"^{}$"), JsonString())
 
 
-def test_any_requires_some_matchers():
+def test_anyof_requires_some_matchers():
     with pytest.raises(TypeError):
         _ = AnyOf()
 
 
-def test_any_warns_on_single_matcher():
+def test_anyof_warns_on_single_matcher():
     with pytest.warns(UserWarning):
         _ = AnyOf(JsonString())
